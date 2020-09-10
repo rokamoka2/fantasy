89b95d0a1c0e9a06ac42b2d9a1d9908acf8eed5c
csaba <csaba@csaba-invitel>
2020-09-10 12:24:16 +0200

    First commit

---
+++ FantasyStats4/src/main/java/com/fantasystats/FantasyStats4Application.java
@@ -0,0 +1,13 @@
+package com.fantasystats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyStats4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyStats4Application.class, args);
+	}
+
+}
---
+++ FantasyStats4/src/test/java/com/fantasystats/FantasyStats4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasystats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyStats4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
